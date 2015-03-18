a2970c91f530aaa81721c0e8861bcb103ff0be6b
kissyteam <tiehang.lth@taobao.com>
2015-03-18 14:12:03 +0800

    sync daily version 3.0.0

---
+++ 3.0.0/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2015-03-18 2:11:42 PM*/
+KISSY.config("modules",{"kg/verify/3.0.0/index":{requires:["node","base","kg/verify/3.0.0/index.css"]}});
---
+++ 3.0.0/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/verify/3.0.0/index': { requires: ['node', 'base', 'kg/verify/3.0.0/index.css']} 
+});
